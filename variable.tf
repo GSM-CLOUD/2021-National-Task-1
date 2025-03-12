7f0396e0fa71b54913c92464929f4cde357e4b95
kimjihoon3106 <kimjihoon3106@gmail.com>
2025-03-12 20:48:31 +0900

    Initial code

---
+++ variable.tf
@@ -0,0 +1,43 @@
+variable "region" {
+  default = "ap-northeast-2"
+}
+
+variable "awscli_profile" {
+  default = "second-profile"
+}
+
+variable "prefix" {
+  default = "wsi"
+}
+
+variable "bucket_custom_prefix" {
+  default = "gsm9"
+}
+
+variable "cluster_name" {
+  default = "skills-ecs-cluster"
+}
+
+variable "service_name" {
+  default = "backend"
+}
+
+variable "container_port" {
+  default = 8080
+}
+
+variable "ecs_task_execution_role_name" {
+    default = "skills-ecs-ecsTaskExecutionRole"
+}
+
+variable "task_definition_name" {
+  default = "skills-task-definition"
+}
+
+variable "container_name" {
+  default = "backend"
+}
+
+variable "default_branch" {
+  default = "main"
+}
